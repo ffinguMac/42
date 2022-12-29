47f484b6b1858872aa9cd817bf244b8baaaa3ab1
Jaewook <parkjaewook228@gmail.com>
2022-12-29 19:02:08 +0900

    ft_ultimate 파일 추가
    다중 포인터

---
+++ c01/ft_ultimate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+
+void main()
+{
+    int a;
+    int *n1, **n2, ***n3, ****n4, *****n5, ******n6, *******n7, ********n8, *********n9;
+    a = 1;
+    n1 = &a;
+    n2 = &n1;
+    n3 = &n2;
+    n4 = &n3;
+    n5 = &n4;
+    n6 = &n5;
+    n7 = &n6;
+    n8 = &n7;
+    n9 = &n8;
+    ft_ultimate_ft(n9);
+    printf("%d", a);
+}
