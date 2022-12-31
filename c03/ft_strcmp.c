51f00275a5122229f49a17a2e8d53b61c6e056cc
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2022-12-31 21:23:37 +0900

    ft_strcmp.c 수정

--- c03/ft_strcmp.c
+++ c03/ft_strcmp.c
@@ -2,21 +2,22 @@
 
 int ft_strcmp(char *s1, char *s2)
 {
-	    int i;
-	        
-	        i = 0;
-		    while(s1[i] == s2[i] && s1[i])
-			        {
-					        i++;
-						    }
-		        return (s1[i] - s2[i]);
+	int i;
+
+	i = 0;
+	while(s1[i] == s2[i] && s1[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+			
 }
 int main(void)
 {
-	    char *str1;
-	        char *str2;
-		    
-		    str1 = "Macguffin";
-		        str2 = "Macgufin";
-			    printf("%d\n", ft_strcmp(str1, str2));
+	char *str1;
+	char *str2;
+
+	str1 = "Macguffin";
+	str2 = "Macgufin";
+	printf("%d\n", ft_strcmp(str1, str2));
 }
