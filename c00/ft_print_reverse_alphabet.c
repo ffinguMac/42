b4e8b56913d50a072e8cb862d20bd28849e5c80c
ffinguMac <parkjaewook228@gmail.com>
2022-12-31 08:25:56 +0000

    ft_print_reverse_alphabet.c

---
+++ c00/ft_print_reverse_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+void ft_print_reverse_alphabet(void)
+{
+    int i;
+
+    i = 122;
+    while(i > 96)
+    {
+        write(1, &i, 1);
+        i--;
+    }
+}
+int main(void)
+{
+    ft_print_reverse_alphabet();
+}
+
