57f349b4e850292853dfad9fa2906cd11e85c8e1
ffinguMac <parkjaewook228@gmail.com>
2022-12-31 08:19:59 +0000

    ft_print_alphabet 파일추가

---
+++ c00/ft_print_alphabet.c
@@ -0,0 +1,16 @@
+nclude <unistd.h>
+void ft_print_alphabet(void)
+{
+    int i;
+
+    i = 97;
+    while(i < 123)
+    {
+        write(1, &i, 1);
+        i++;
+    }
+}
+int main(void)
+{
+    ft_print_alphabet();
+}
